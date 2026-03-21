bbb92c4ad9e0de85c114561da2577f38eb9c8bbf
OBINNA <anyanwuclems@gmail.com>
2026-03-21 13:49:13 +0100

    up

--- src/components/WorkGrid.tsx
+++ src/components/WorkGrid.tsx
@@ -81,7 +81,7 @@ export const projects = [
     year: "2025-2026",
     images: [
       "/fimacasestudy/cover.webp",
-      "/fimacasestudy/cover2.webp",
+      "/fimacasestudy/coverr.webp"
       
     ],
     description: "Designed a user-friendly utility app that enables users to buy data, airtime, electricity, and cable subscriptions seamlessly",
